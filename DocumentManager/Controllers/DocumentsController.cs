350ef3f9398d9f51bc1d2bb5f5f376559488894e
David González <nodearth@gmail.com>
2017-10-16 00:39:06 +0100

    Working

---
+++ DocumentManager/Controllers/DocumentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DocumentManager.Controllers.Domain;
+using Pomelo.Data.MySql;
+using System.Data;
+
+namespace DocumentManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class DocumentsController : Controller
+    {
+        // GET api/documents
+        [HttpGet]
+        public ArrayList Get()
+        {
+            string connectionString = @"server=127.0.0.1;" +
+                 @"uid=root;" +
+                 @"pwd=root;" +
+                 @"database=documents_db;";
+            IDbConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            ArrayList results = new ArrayList();
+            try {
+                IDbCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT title, page_count FROM documents;";
+
+                IDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    results.Add(new Document((string)reader["title"], (int)reader["page_count"]));
+                }    
+            } finally {
+                connection.Close();    
+            }
+            return results;
+        }
+    }
+}
---
+++ DocumentManager/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DocumentManager.Controllers.Domain
+{
+    public class Document
+    {
+        public string title { get; set; }
+        public int pageCount { get; set; }
+
+        public Document()
+        {
+        }
+
+        public Document(String title, int pageCount) {
+            this.title = title;
+            this.pageCount = pageCount;
+        }
+    }
+}
